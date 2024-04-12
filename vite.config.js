da1b685197b5c5c3299fe680fe2e80143728ab5a
Gabriel Oliveira <c.gabriel.oliveira.p@gmail.com>
2024-04-13 00:12:32 +0100

    feat: add base styles

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/variables.scss";'
+      }
+    }
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.scss']
   }
 })
