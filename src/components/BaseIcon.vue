4694f33d66ff264c3800dd342f69c64fb954cb8a
Gabriel Oliveira <c.gabriel.oliveira.p@gmail.com>
2024-04-13 14:10:05 +0100

    feat: add BaseIcon component

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    v-if="icon"
+    class="base-icon"
+  >
+    <template v-if="icon">
+      <component
+        :is="icon"
+        v-if="color"
+        :color="color"
+      />
+      <component
+        :is="icon"
+        v-else
+      />
+    </template>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+import arrowUp from '@/assets/icons/arrow-up.svg';
+import arrowDown from '@/assets/icons/arrow-down.svg';
+import book from '@/assets/icons/book.svg';
+import calendar from '@/assets/icons/calendar.svg';
+import clock from '@/assets/icons/clock.svg';
+import download from '@/assets/icons/download.svg';
+import search from '@/assets/icons/search.svg';
+import togglerBlock from '@/assets/icons/toggler-block.svg';
+
+
+const availableIcons = {
+  'arrow-up': arrowUp,
+  'arrow-down': arrowDown,
+  book,
+  calendar,
+  clock,
+  download,
+  search,
+  'toggler-block': togglerBlock
+};
+
+const props = defineProps({
+  color: {
+    default: null,
+    type: String
+  },
+  name: {
+    default: null,
+    type: String
+  }
+});
+
+
+// Computed
+
+const icon = computed(() => availableIcons[props.name]);
+
+</script>
+
+<style lang="scss">
+.base-icon {
+  display: inline-flex;
+  min-width: $layout-unit-md;
+  min-height: $layout-unit-md;
+  height: $layout-unit-md;
+
+  //.base-icon svg
+  svg {
+    height: 100%;
+    width: auto;
+  }
+}
+</style>
