a2f62c514f7a05afd0ab139c1c7be30042fd418a
Gabriel Oliveira <c.gabriel.oliveira.p@gmail.com>
2024-04-13 17:20:33 +0100

    feat(components): add BasePanel component

---
+++ src/components/BasePanel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="base-panel"
+    :class="classes"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const AVAILABLE_PADDING_SIZES = ['small', 'default', 'large'];
+
+
+const props = defineProps({
+  paddingSize: {
+    default: 'default',
+    type: String
+  }
+});
+
+const classes = computed(() => {
+  const classesObject = {};
+
+  if (
+    !!props.paddingSize &&
+    AVAILABLE_PADDING_SIZES.includes(props.paddingSize) &&
+    props.paddingSize !== 'default'
+  ) classesObject[`base-panel--${props.paddingSize}-padding`] = true;
+
+  return classesObject;
+});
+</script>
+
+<style lang="scss" scoped>
+.base-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  padding: $layout-unit-md;
+
+  background-color: $color-white;
+
+  border-radius: 5px;
+
+  // .base-panel--large-padding
+  &--large-padding {
+    padding: $layout-unit-lg;
+  }
+
+  // .base-panel--small-padding
+  &--small-padding {
+    padding: $layout-unit-sm;
+  }
+}
+</style>
