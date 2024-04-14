5cd733a5990c7eb4e155a5ab80a62ef7fa31d6b1
Gabriel Oliveira <c.gabriel.oliveira.p@gmail.com>
2024-04-14 23:11:36 +0100

    feat(main.js): add PrimeVue to app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import PrimeVue from 'primevue/config';
+
 import App from '@/App.vue';
 import router from '@/router';
 
@@ -10,5 +12,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue, { unstyled: true });
 
 app.mount('#app');
